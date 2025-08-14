26322fbcd45d1a16cfee249a5a4bded7f6bb1153
ssdf <myneww00@gmail.com>
2025-08-14 06:29:49 +0000

    ؤؤءبرلsss

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9M4S1blhwbJQ5FUXBkqzv5lsE_3QR4mQ",
-  authDomain: "amer-98afb.firebaseapp.com",
-  projectId: "amer-98afb",
-  storageBucket: "amer-98afb.firebasestorage.app",
-  messagingSenderId: "492435134855",
-  appId: "1:492435134855:web:86d3abd762bdc1856e5b22",
-  measurementId: "G-1YBB0DGJ9L",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
